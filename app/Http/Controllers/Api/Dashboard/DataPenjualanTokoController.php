20acf412b45bdaf15fbe41463c42c511adbf62b4
pujiermanto <pujiermanto@gmail.com>
2024-01-12 09:37:27 +0700

    update

---
+++ app/Http/Controllers/Api/Dashboard/DataPenjualanTokoController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Api\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use App\Events\{EventNotification};
+use App\Helpers\{WebFeatureHelpers};
+use App\Http\Resources\{ResponseDataCollect, RequestDataCollect};
+use App\Models\{Penjualan,ItemPenjualan,Pelanggan,Barang,Kas};
+use Auth;
+
+class DataPenjualanTokoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        try {
+            $keywords = $request->query('keywords');
+            $today = now()->toDateString();
+
+            $query = Penjualan::query()
+            ->select(
+                'penjualan.*',
+                'itempenjualan.*',
+                'pelanggan.nama as nama_pelanggan',
+                'pelanggan.alamat as alamat_pelanggan'
+            )
+            ->leftJoin('itempenjualan', 'penjualan.kode', '=', 'itempenjualan.kode')
+            ->leftJoin('pelanggan', 'penjualan.pelanggan', '=', 'pelanggan.kode')
+            ->orderByDesc('penjualan.id')
+            ->limit(10);
+
+            if ($keywords) {
+                $query->where('penjualan.kode', 'like', '%' . $keywords . '%');
+            }
+
+            $query->whereDate('penjualan.tanggal', '=', $today);
+
+            $penjualans = $query->orderByDesc('penjualan.id')
+            ->paginate(10);
+
+            return new ResponseDataCollect($penjualans);
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
