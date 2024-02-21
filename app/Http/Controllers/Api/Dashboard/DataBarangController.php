9c9ac79b0b701272110a86f34bd815d6b11f4721
root <root@digitalkreatifsolusindo.cc>
2024-02-21 11:30:49 +0700

    updated

--- app/Http/Controllers/Api/Dashboard/DataBarangController.php
+++ app/Http/Controllers/Api/Dashboard/DataBarangController.php
@@ -402,7 +402,14 @@ class DataBarangController extends Controller
             }
 
             $newBarang->kode_barcode = $newBarang->kode;
-            $newBarang->tgl_terakhir = $request->tglbeli;
+
+            if ($request->tglbeli !== "null") {
+                    $tgl_terakhir = Carbon::createFromFormat('Y-m-d', $request->tglbeli)->format('Y-m-d');
+            } else {
+                $tgl_terakhir = NULL;
+            }
+
+            $newBarang->tgl_terakhir = $tgl_terakhir;
             $newBarang->ket = $request->keterangan ? ucfirst(htmlspecialchars($request->keterangan)) : NULL;
 
             $newBarang->save();
