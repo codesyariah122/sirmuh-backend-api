8976c6f96fce9df67d645497a3c428311fbb0b08
root <root@digitalkreatifsolusindo.cc>
2024-02-24 18:46:27 +0700

    update aje

--- app/Http/Controllers/Api/Dashboard/DataMutasiKasController.php
+++ app/Http/Controllers/Api/Dashboard/DataMutasiKasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Dashboard;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
@@ -11,7 +12,7 @@ use Illuminate\Support\Facades\Validator;
 use App\Events\{EventNotification};
 use App\Helpers\{WebFeatureHelpers};
 use App\Http\Resources\{ResponseDataCollect, RequestDataCollect};
-use App\Models\{MutasiKas};
+use App\Models\{MutasiKas, Kas};
 use Auth;
 
 class DataMutasiKasController extends Controller
@@ -51,7 +52,56 @@ class DataMutasiKasController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'kas_id' => 'required',
+                'jumlah' => 'required',
+                'destination' => 'required'
+            ]);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+
+            $kas_id = $request->kas_id;
+            $destination = $request->destination;
+            $jumlah = $request->jumlah;
+
+            $ownKas = Kas::findOrFail($kas_id);
+            $ownKas->saldo = intval($ownKas->saldo) - intval($jumlah);
+            $ownKas->save();
+
+            $destKas = Kas::findOrFail($destination);
+            $destKas->saldo = intval($destKas->saldo) + intval($jumlah);
+            $destKas->save();
+
+            $userOnNotif = Auth::user();
+
+            $data_event = [
+                'routes' => 'mutasi-kas',
+                'alert' => 'success',
+                'type' => 'add-data',
+                'notif' => "Kas {$ownKas->nama}, mutasi successfully 🤙!",
+                'data' => $destKas,
+                'user' => $userOnNotif
+            ];
+
+            event(new EventNotification($data_event));
+
+            $newOwnKas = Kas::findOrFail($kas_id);
+            $newDestKas = Kas::findOrFail($destination);
+
+            return response()->json([
+                'success' => true,
+                'message' => "Mutasi kas {$ownKas->nama}, successfully ✨",
+                'data' => [
+                    'own_kas' => $newOwnKas,
+                    'dest_kas' => $newDestKas
+                ]
+            ], 200);
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
