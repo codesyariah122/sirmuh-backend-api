57dfa7c79d278299cc1a7356686a4190f497eae5
root <root@digitalkreatifsolusindo.cc>
2024-02-27 13:21:20 +0700

    update

--- app/Http/Controllers/Api/Dashboard/DataWebFiturController.php
+++ app/Http/Controllers/Api/Dashboard/DataWebFiturController.php
@@ -1734,8 +1734,8 @@ class DataWebFiturController extends Controller
                         $updateExistingItem = ItemPenjualan::findOrFail($existingItem->id);
                             // Jika sudah ada, update informasi yang diperlukan
                         $updateExistingItem->qty = intval($barang['qty']);
-                        $updateExistingItem->harga_beli = intval($barang['harga_toko']);
-                        $updateExistingItem->subtotal = $barang['harga_toko'] * $barang['qty'];
+                        $updateExistingItem->harga_beli = intval($barang['harga']);
+                        $updateExistingItem->subtotal = $barang['harga'] * $barang['qty'];
                             // Update atribut lainnya sesuai kebutuhan
                         $updateExistingItem->save();
                         $lastItemPembelianId = $updateExistingItem->id;
