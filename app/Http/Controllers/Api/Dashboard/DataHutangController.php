3f7b9488d624d070f7e5b80b972729f8ebeb5aab
root <root@digitalkreatifsolusindo.cc>
2024-04-06 06:12:22 +0700

    update juga

--- app/Http/Controllers/Api/Dashboard/DataHutangController.php
+++ app/Http/Controllers/Api/Dashboard/DataHutangController.php
@@ -89,7 +89,7 @@ class DataHutangController extends Controller
             if ($viewAll === true || $viewAll === "true") {
                 $query->whereBetween('hutang.tanggal', [$startOfMonth, $endOfMonth]);    
             }  else {
-                $query->limit(10);
+                $query->paginate(10);
             }
 
             if ($keywords) {
