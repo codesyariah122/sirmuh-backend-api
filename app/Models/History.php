1ab9ae1dd15820eed994362783779f3988a6458e
codesyariah122 <pujiermanto@gmail.com>
2024-05-31 02:18:27 +0700

    update dulu deh

--- app/Models/History.php
+++ app/Models/History.php
@@ -6,11 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Histori extends Model
+class History extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory; use SoftDeletes;
 
     protected $table = 'histori';
-
 }
